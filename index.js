29864618dd028283fa4d18b7083d3a1e4055d62e
macxwell <omondimacxwell@gmail.com>
2024-07-19 09:28:19 +0300

    first attempt of scope lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+
+
+let customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+let bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
